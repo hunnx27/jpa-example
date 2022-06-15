e684e5daae573ed9d3d88b2a3fbdca9f0e5ccaf1
최지훈 <hunnx27@i-screamkids.com>
2022-06-15 15:00:00 +0900

    feat: 익센션 처리 추가

---
+++ src/main/java/com/example/jpaexample/common/advice/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.example.jpaexample.common.advice;
+
+import com.example.jpaexample.common.enums.ErrorCode;
+import com.example.jpaexample.common.vo.exception.CustomException;
+import com.example.jpaexample.common.vo.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * 사용자 정의 Exception
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    /**
+     *
+     * Exception들 추가 필요...
+     * ...
+     * ...
+     * ...+
+     */
+
+
+    /**
+     * 공통 Exception
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ErrorResponse> handleAllException(Exception ex) {
+        log.warn("handleAllException", ex);
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ErrorResponse.toResponseEntity(errorCode);
+    }
+
+}
---
+++ src/main/java/com/example/jpaexample/common/vo/response/ErrorResponse.java
@@ -0,0 +1,33 @@
+package com.example.jpaexample.common.vo.response;
+
+import com.example.jpaexample.common.enums.ErrorCode;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final String error;
+    private final String code;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getHttpStatus().value())
+                        .error(errorCode.getHttpStatus().name())
+                        .code(errorCode.name())
+                        .message(errorCode.getDetail())
+                        .build()
+                );
+    }
+}
