0106f78c24af55c93946594fef176c377019f8b6
최지훈 <hunnx27@i-screamkids.com>
2022-06-16 14:40:47 +0900

    fix: 도메인 누락된 어노테이션 추가

--- src/main/java/com/example/jpaexample/modules/user/domain/User.java
+++ src/main/java/com/example/jpaexample/modules/user/domain/User.java
@@ -1,13 +1,21 @@
 package com.example.jpaexample.modules.user.domain;
 
+import com.example.jpaexample.modules.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class User {
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
 }
